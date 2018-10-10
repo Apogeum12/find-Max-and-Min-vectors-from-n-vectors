5e10a03580ea9dece23a5a69eaa1b901b8908894
Sebastian Łuc <koemirai@gmail.com>
2018-10-11 01:12:43 +0200

    I connected two in one function in MaxMinA()

--- MaxMinVec/MaxMinVec.cpp
+++ MaxMinVec/MaxMinVec.cpp
@@ -2,60 +2,50 @@
 
 MaxMinVec::MaxMinVec(std::vector<std::vector<double>> A) {
 	this->A = A;
-	this->searchMaxA();
-	this->searchMinA();
+	this->searchMaxMinA();
 }
 
-void MaxMinVec::searchMaxA() {
+void MaxMinVec::searchMaxMinA() {
 	size_t size_row = A.size();
 		std::vector<double> max_a;
-		double compare;
-		double sum = 0;
-		int ite = 0;
+		std::vector<double> min_a;
+		double compare1, compare2;
+		double sum1 = 0, sum2 = 0;
+		int ite1 = 0, ite2 = 0;
 		for (int i = 0; i < size_row; i++) {
 			size_t size_col = A.at(i).size();
 			for (int j = 0; j < size_col; j++) {
-				sum += A.at(i).at(j)*A.at(i).at(j);
+				sum1 += A.at(i).at(j)*A.at(i).at(j);
+				sum2 += A.at(i).at(j)*A.at(i).at(j);
 			}
-			max_a.push_back(sum);
-			sum = 0;
+			max_a.push_back(sum1);
+			min_a.push_back(sum2);
+			sum1 = 0;
+			sum2 = 0;
 		}
-		compare = sqrt(max_a.at(0));
+	
+		compare1 = sqrt(max_a.at(0));
+		compare2 = sqrt(min_a.at(0));
 		for (int i = 0; i < max_a.size(); i++) {
-			if (compare >= sqrt(max_a.at(i))) ite;
-			else { ite = i; compare = sqrt(max_a.at(i)); }
+			if (compare1 >= sqrt(max_a.at(i))) ite1;
+			else { ite1 = i; compare1 = sqrt(max_a.at(i)); }
+		}
+		
+		for (int i = 0; i < min_a.size(); i++) {
+			if (compare2 <= sqrt(min_a.at(i))) ite2;
+			else { ite2 = i; compare2 = sqrt(min_a.at(i)); }
 		}
 
-	for (int i = 0; i < this->A.at(ite).size(); i++) {
-		this->MaxA.push_back(this->A.at(ite).at(i));
+	for (int i = 0; i < this->A.at(ite1).size(); i++) {
+		this->MaxA.push_back(this->A.at(ite1).at(i));
 	}
-	max_a.clear();
-}
-
-void MaxMinVec::searchMinA() {
-	size_t size_row = A.size();
-	std::vector<double> min_a;
-	double compare = 0;
-	double sum = 0;
-	int ite = 0;
-	for (int i = 0; i < size_row; i++) {
-		size_t size_col = A.at(i).size();
-		for (int j = 0; j < size_col; j++) {
-			sum += A.at(i).at(j)*A.at(i).at(j);
-		}
-		min_a.push_back(sum);
-		sum = 0;
+	
+	for (int i = 0; i < this->A.at(ite2).size(); i++) {
+		this->MinA.push_back(this->A.at(ite2).at(i));
 	}
-	compare = sqrt(min_a.at(0));
-	for (int i = 0; i < min_a.size(); i++) {
-		if (compare <= sqrt(min_a.at(i))) ite;
-		else { ite = i; compare = sqrt(min_a.at(i)); }
-	}
-
-	for (int i = 0; i < this->A.at(ite).size(); i++) {
-		this->MinA.push_back(this->A.at(ite).at(i));
-	}
+	
 	min_a.clear();
+	max_a.clear();
 }
 
 void MaxMinVec::printToConsole() {
